8a1c03ddede9a0b8ba54c1e9d69105abf260aa9a
Julio Oliveira <antoniojuliodeo@gmail.com>
2018-07-26 09:12:14 -0300

    Exercise 1-2

---
+++ 1-introduction/e1-2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+
+main () {
+  printf("hello, world\h");
+}
+
+/*
+  usando h como espace sequence:
+  warning: sequência de escape desconhecida: "\h"
+
+  out:
+  "hello, worldh"
+*/
